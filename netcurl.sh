136460bc642fac6dc4285bc8b37b5fdc19652877
Daniel Mauser <dmauser@hotmail.com>
2025-04-08 09:59:08 -0500

    update

--- netcurl.sh
+++ netcurl.sh
@@ -1,10 +1,23 @@
 #! /bin/bash
+echo "Testing connectivity to various IP addresses using curl"
+echo "--------------------------------------------------"
+echo "10.100.0.4"
+curl 10.100.0.4
+echo "10.110.0.4"
 curl 10.110.0.4
+echo "10.120.0.4"
 curl 10.120.0.4
+echo "172.16.100.4"
 curl 172.16.100.4
+echo "172.16.3.4"
 curl 172.16.3.4
+echo "172.16.4.4"
 curl 172.16.4.4
+echo "172.16.5.4"
 curl 172.16.5.4
+echo "172.16.6.4"
 curl 172.16.6.4
+echo "172.16.2.4"
 curl 172.16.2.4
+echo "172.16.1.4"
 curl 172.16.1.4
--- netping.sh
+++ netping.sh
@@ -1,10 +1,21 @@
 #! /bin/bash
+echo "Testing connectivity to various IP addresses using ping"
+echo "--------------------------------------------------"
+echo "Pinging 10.110.0.4"
 ping -O -c 5 10.110.0.4
+echo "Pinging 10.120.0.4"
 ping -O -c 5 10.120.0.4
+echo "Pinging 172.16.100.4"
 ping -O -c 5 172.16.100.4
+echo "Pinging 172.16.3.4"
 ping -O -c 5 172.16.3.4
+echo "Pinging 172.16.4.4"
 ping -O -c 5 172.16.4.4
+echo "Pinging 172.16.5.4"
 ping -O -c 5 172.16.5.4
+echo "Pinging 172.16.6.4"
 ping -O -c 5 172.16.6.4
+echo "Pinging 172.16.2.4"
 ping -O -c 5 172.16.2.4
+echo "Pinging 172.16.1.4"
 ping -O -c 5 172.16.1.4
