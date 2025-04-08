b355d2e7e175e73fc36dc63eb25a14bb8c7683e2
Daniel Mauser <dmauser@hotmail.com>
2025-04-08 10:21:28 -0500

    Update netcurl.sh

--- netcurl.sh
+++ netcurl.sh
@@ -2,22 +2,22 @@
 echo "Testing connectivity to various IP addresses using curl"
 echo "--------------------------------------------------"
 echo "10.100.0.4"
-curl --max-time 0.005 10.100.0.4
+curl --max-time 5 10.100.0.4
 echo "10.110.0.4"
-curl --max-time 0.005 10.110.0.4
+curl --max-time 5 10.110.0.4
 echo "10.120.0.4"
-curl --max-time 0.005 10.120.0.4
+curl --max-time 5 10.120.0.4
 echo "172.16.100.4"
-curl --max-time 0.005 172.16.100.4
+curl --max-time 5 172.16.100.4
 echo "172.16.3.4"
-curl --max-time 0.005 172.16.3.4
+curl --max-time 5 172.16.3.4
 echo "172.16.4.4"
-curl --max-time 0.005 172.16.4.4
+curl --max-time 5 172.16.4.4
 echo "172.16.5.4"
-curl --max-time 0.005 172.16.5.4
+curl --max-time 5 172.16.5.4
 echo "172.16.6.4"
-curl --max-time 0.005 172.16.6.4
+curl --max-time 5 172.16.6.4
 echo "172.16.2.4"
-curl --max-time 0.005 172.16.2.4
+curl --max-time 5 172.16.2.4
 echo "172.16.1.4"
-curl --max-time 0.005 172.16.1.4
+curl --max-time 5 172.16.1.4
