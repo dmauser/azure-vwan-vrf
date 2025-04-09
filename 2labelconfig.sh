ccdd69ce1ae4fc7b87cf2b95088ddb80abafeb3e
Daniel Mauser <dmauser@hotmail.com>
2025-04-08 20:44:35 -0500

    Update 2labelconfig.sh

--- 2labelconfig.sh
+++ 2labelconfig.sh
@@ -19,13 +19,15 @@ start=`date +%s`
 echo "Script started at $(date)"
 
 
-# Create vHubs Labels
+# Configuring labels for vHUBs...
+echo Configuring labels for vHUBs...
 az network vhub route-table update -n defaultroutetable -g $rg --vhub-name $hub1name --labels default prod --no-wait
 az network vhub route-table update -n defaultroutetable -g $rg --vhub-name $hub2name --labels default dev --no-wait
 az network vhub route-table update -n defaultroutetable -g $rg --vhub-name $hub3name --labels default vendor1 --no-wait
 az network vhub route-table update -n defaultroutetable -g $rg --vhub-name $hub4name --labels default vendor2 --no-wait
 
 # Update VNET connections
+echo Configuring labels for VNET connections...
 az network vhub connection update --name dmz-vnetconn --vhub-name $hub1name --resource-group $rg --labels prod --no-wait
 az network vhub connection update --name sd-wan-prodconn --vhub-name $hub1name --resource-group $rg --labels prod vendor1 vendor2 --no-wait
 az network vhub connection update --name sd-wan-devconn --vhub-name $hub2name --resource-group $rg --labels dev vendor1 vendor2 --no-wait
@@ -33,12 +35,14 @@ az network vhub connection update --name hub3-fw-vnetconn --vhub-name $hub3name 
 az network vhub connection update --name hub4-fw-vnetconn --vhub-name $hub4name --resource-group $rg --labels vendor2 prod dev --no-wait
 
 # VPN Connections Labels (This is required to isolate branches.)
+echo Configuring labels for VPN connections...
 default_hub1=$(az network vhub route-table show --name defaultroutetable --vhub-name $hub1name -g $rg --query id -o tsv)
 default_hub2=$(az network vhub route-table show --name defaultroutetable --vhub-name $hub2name -g $rg --query id -o tsv)
 az network vpn-gateway connection update --gateway-name $hub1name-vpngw -n connection-site-branch1 -g $rg --propagated $default_hub1 --label prod vendor1 vendor2 --output none --no-wait
 az network vpn-gateway connection update --gateway-name $hub2name-vpngw -n connection-site-branch2 -g $rg --propagated $default_hub2 --label dev vendor1 vendor2 --output none --no-wait
 
 # Apply iptables rules to NVA
+echo Configuring iptables rules on DMZ-NVA...
 scripturi="https://raw.githubusercontent.com/dmauser/azure-vwan-vrf/refs/heads/main/scripts/iptables.sh"
 az vm run-command invoke -g $rg -n dmz-nva --command-id RunShellScript --scripts "curl -s $scripturi | bash" --output none --no-wait
 
